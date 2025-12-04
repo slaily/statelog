d54c4aceee1f185d75ffc6072a04861bfd19e185
slaily <slavov.iliyan96@gmail.com>
2025-12-04 17:30:36 +0200

    Add func to create WAL file

--- statelog.go
+++ statelog.go
@@ -1,5 +1,11 @@
 package statelog
 
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
 type Statelog struct {
 	DirPath        string
 	BufferCapacity int
@@ -8,19 +14,55 @@ type Statelog struct {
 }
 
 func NewStatelog(dirPath string, bufferCapacity int) Statelog {
-	return Statelog{
+	sl := Statelog{
 		DirPath:        dirPath,
 		BufferCapacity: bufferCapacity,
 		states:         make([]string, bufferCapacity),
 		length:         0,
 	}
+	sl.ensureDirExists()
+
+	return sl
 }
 
-func (statelog *Statelog) Append(data string) {
-	if statelog.length >= len(statelog.states) {
-		statelog.states = append(statelog.states, data)
+func (sl *Statelog) Append(data string) {
+	if sl.length >= len(sl.states) {
+		sl.states = append(sl.states, data)
 	} else {
-		statelog.states[statelog.length] = data
+		sl.states[sl.length] = data
 	}
-	statelog.length++
+	sl.length++
+}
+
+func (sl *Statelog) ensureDirExists() error {
+	_, err := os.Stat(sl.DirPath)
+
+	if err != nil {
+		err := os.MkdirAll(sl.DirPath, 0755)
+
+		if err != nil {
+			return fmt.Errorf("failed to create statelog directory: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (sl *Statelog) ensureWALFileExists() error {
+	walPath := path.Join(sl.DirPath, "wal.log")
+	_, err := os.Stat(walPath)
+
+	if err == nil {
+		return nil
+	}
+
+	file, err := os.OpenFile(walPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+
+	if err != nil {
+		return fmt.Errorf("failed to create statelog WAL file: %w", err)
+	}
+
+	defer file.Close()
+
+	return nil
 }
